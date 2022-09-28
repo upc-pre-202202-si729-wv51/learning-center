f28db5d0c3e5f04ebc86227d9b2d75b7db47b735
Angel Velasquez <pcisavel@upc.edu.pe>
2022-09-27 21:11:49 -0500

    refactor: Cleaned up Students Service.

--- src/app/learning/services/students.service.ts
+++ src/app/learning/services/students.service.ts
@@ -1,69 +1,18 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
-import {catchError, Observable, retry, throwError} from "rxjs";
-import {Student} from "../model/student";
+import { HttpClient } from "@angular/common/http";
+import { Student } from "../model/student";
+import { ResourceService } from "../../shared/services/resource.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class StudentsService {
+export class StudentsService extends ResourceService<Student> {
 
   // Students Endpoint
-  basePath = 'http://localhost:3000/api/v1/students';
 
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
-  };
-
-  constructor(private http: HttpClient) { }
-
-  // API Error Handling
-  handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      // Default Error Handling
-      console.log(`An error occurred: ${error.error}`);
-    } else {
-      // Unsuccessful Response Error Code returned from Backend
-      console.error(`Backend returned code ${error.status}, body was: ${error.error}`);
-    }
-    // Return observable with Error Message to Client
-    return throwError(() =>
-      new Error('Something happened with request, please try again later'));
+  constructor(http: HttpClient) {
+    super(http);
+    this.basePath = 'http://localhost:3000/api/v1/students';
   }
 
-  // Create Student
-  create(item: any): Observable<Student> {
-    return this.http.post<Student>(
-      this.basePath,
-      JSON.stringify(item),
-      this.httpOptions)
-      .pipe(retry(2),
-        catchError(this.handleError));
-  }
-
-  // Get Student by id
-  getById(id: any): Observable<Student> {
-    return this.http.get<Student>(`${this.basePath}/${id}`, this.httpOptions)
-      .pipe(retry(2), catchError(this.handleError));
-  }
-
-  // Get All Students
-  getAll(): Observable<Student> {
-    return this.http.get<Student>(this.basePath, this.httpOptions)
-      .pipe(retry(2), catchError(this.handleError));
-  }
-
-  // Update Student
-  update(id: any, item: any): Observable<Student> {
-    return this.http.put<Student>(`${this.basePath}/${id}`, JSON.stringify(item), this.httpOptions)
-      .pipe(retry(2), catchError(this.handleError));
-  }
-
-  // Delete Student
-  delete(id: any): Observable<Student> {
-    return this.http.delete<Student>(`${this.basePath}/${id}`, this.httpOptions)
-      .pipe(retry(2), catchError(this.handleError));
-  }
 }
